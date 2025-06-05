3407b3a22a880e0b83c41a9da80eb76572f24aed
96kixx <96kixx@users.noreply.github.com>
2025-06-05 14:33:59 +0000

    deploy: 29fb5fa150989b34c12b1aa031796ff115edb2bf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -335,7 +335,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "34beda9f39eb7d992d46125ca868dc61",
 "canvaskit/chromium/canvaskit.wasm": "64a386c87532ae52ae041d18a32a3635",
 "canvaskit/chromium/canvaskit.js.symbols": "5a23598a2a8efd18ec3b60de5d28af8f",
-"flutter_bootstrap.js": "05199a554d325cdda04571b45ea982df",
+"flutter_bootstrap.js": "ac3aac799ab985883659e3cfabe2dc6f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "6c167ee40d3ce57d3d212da532398213",
 "/": "6c167ee40d3ce57d3d212da532398213",
